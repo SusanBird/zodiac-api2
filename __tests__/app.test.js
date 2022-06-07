339fbb58dfff4a0c592112b3c8aa4ab292c3cf46
SusanBird <susanbird391@gmail.com>
2022-06-06 18:29:20 -0700

    first test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,16 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { get } = require('../lib/app');
+const { zodiacs } = require('../data/zodiacs');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('should return a list of zodiacs', () => {
+    const res = await request(app).get('/zodiacs');
+    expect(res.body).toEqual(zodiacs);
   });
   afterAll(() => {
     pool.end();
